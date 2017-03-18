6d499f52cad4beda212640f3f7781a60090d24af
karwankarwan <lkarwan95@gmail.com>
2017-03-18 15:32:19 +0800

    this is a message

---
+++ MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.gymappassignment;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+        LinearLayout ll1 = new LinearLayout(this);
+
+        TextView tv1 = new TextView(this);
+        tv1.setText("hello from github, and kw. ");
+        TextView tv2 = new TextView(this);
+        tv2.setText("let say i add one line of code here ");
+        //ok let say i add a comment here
+        
+        ll1.addView(tv1);
+        
+        setContentView(ll1);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
