49b2228f8f00215e0dcac869b5876ed057853108
karwankarwan <lkarwan95@gmail.com>
2017-03-18 17:11:16 +0800

    1

--- MainActivity.java
+++ MainActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,14 +21,60 @@ public class MainActivity extends ActionBarActivity {
 
         
         LinearLayout ll1 = new LinearLayout(this);
+        ll1.setOrientation(LinearLayout.VERTICAL);
 
         TextView tv1 = new TextView(this);
         tv1.setText("hello from github, and kw. ");
         TextView tv2 = new TextView(this);
         tv2.setText("let say i add one line of code here ");
         //ok let say i add a comment here
-        
+
         ll1.addView(tv1);
+        ll1.addView(tv2);
+        
+        
+        final EditText et1 = new EditText(this);
+        
+        ll1.addView(et1);      
+
+        MySqlHelper mysqlhelper;
+        
+        Button bt1 = new Button(this);
+        bt1.setText("Submit");   
+        bt1.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(et1.getText().toString().matches("")){
+					Toast.makeText(MainActivity.this, "text", Toast.LENGTH_SHORT).show();
+				}
+				else{
+					int value = Integer.valueOf(et1.getText().toString());
+					
+					MySqlHelper mysqlhelper;
+					
+			        mysqlhelper = new MySqlHelper(MainActivity.this);
+			        mysqlhelper.openToWrite();
+			        mysqlhelper.insert(value);
+			        mysqlhelper.close();
+				}
+				
+				
+			}
+        });
+        ll1.addView(bt1);     
+        
+        
+
+        
+        mysqlhelper = new MySqlHelper(this);
+        mysqlhelper.openToRead();
+        String mycontent = mysqlhelper.queueAll();
+        mysqlhelper.close();
+        
+        
+        
+        
+        
         
         setContentView(ll1);
         
